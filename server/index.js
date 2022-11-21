904356f7df2bb3afbb789c8dcf800834b5dabf5c
Jenni Hautala <jennihau@Jennis-MacBook-Pro.local>
2022-11-21 16:52:43 +0200

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose, { mongo } from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://patalapanen:lapapatanen@clustersome.1s8qkoz.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.port || 5001;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// https://www.mongodb.com/cloud/atlas
